0e7542243ecf68a38d6e20c33a53ca89d44af993
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2025-11-21 11:35:05 +0530

    added responsiveness

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import { Menu } from 'lucide-react';
 export default function Navbar() {
   const [open, setOpen] = useState(false);
   const [hidden, setHidden] = useState(false);
@@ -9,17 +9,17 @@ export default function Navbar() {
   const handleScroll = () => {
     const currentScrollY = window.scrollY;
 
-    if (currentScrollY > window.innerHeight){
+    if (currentScrollY > window.innerHeight) {
       setBorder(true);
     } else {
       setBorder(false);
     }
 
-    
+
     if (currentScrollY > 180 && currentScrollY > lastScrollY) {
-      setHidden(true); 
+      setHidden(true);
     } else {
-      setHidden(false); 
+      setHidden(false);
     }
 
     setLastScrollY(currentScrollY);
@@ -33,10 +33,9 @@ export default function Navbar() {
 
   return (
     <nav
-  className={`w-full fixed top-0 left-0 bg-[#0d1117] text-white px-8 py-4 flex justify-between items-center z-50 transform transition-transform duration-300 ${
-    hidden ? "-translate-y-full" : "translate-y-0"
-  } ${border ? "border-b border-1.5 border-gray-800" : ""}`}
->
+      className={`w-full fixed top-0 left-0 bg-[#0d1117] text-white px-8 py-4 flex justify-between items-center z-50 transform transition-transform duration-300 ${hidden ? "-translate-y-full" : "translate-y-0"
+        } ${border ? "border-b border-1.5 border-gray-800" : ""}`}
+    >
 
       <h1 className="text-2xl tracking-tight font-grotesk">Saurabh<span className="text-gray-500">
         _Commits</span></h1>
@@ -60,11 +59,22 @@ export default function Navbar() {
       </ul>
 
       <button className="md:hidden" onClick={() => setOpen(!open)}>
-        ☰
+        <Menu size={24} />
       </button>
 
       {open && (
-        <div className="absolute right-8 top-16 bg-[#0d1117] p-4 rounded-lg flex flex-col gap-4 md:hidden">
+        <div
+          className="
+      absolute right-2 top-18 bg-[#0d1117] p-4 rounded-lg flex flex-col gap-4 md:hidden font-inter
+      transition-all duration-300 ease-out
+      opacity-100 translate-y-0 border border-[#1f2b3f] 
+    "
+          style={{
+            animation: open
+              ? 'slideFadeIn 0.3s ease-out forwards'
+              : 'slideFadeOut 0.3s ease-in forwards',
+          }}
+        >
           <a href="#about">About</a>
           <a href="#projects">Projects</a>
           <a href="#testimonials">Testimonials</a>
@@ -72,6 +82,7 @@ export default function Navbar() {
           <a href="#contact">Contact</a>
         </div>
       )}
+
     </nav>
   );
 }
